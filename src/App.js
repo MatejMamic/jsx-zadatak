2a123ffb7da7267367233f77f5e4773dea57e143
Matej Mamic <matejmamic10@gmail.com>
2024-04-22 20:36:40 +0200

    jsx zadatak

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const ime = "Matej";
+  const prezime = "Mamic";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Ovo je moj prvi jsx zadatak</p>
+      <p>Moje ime je:{ime}</p>
+      <p>
+        Moje ime i prezime je {ime} {prezime}
+      </p>
     </div>
   );
 }
